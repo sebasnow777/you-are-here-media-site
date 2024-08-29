09f7f4364b700cf46b1b97bfe2fe1ee4b2d9c050
Sebastian Nowicki <108746110+sebasnow777@users.noreply.github.com>
2024-08-29 17:31:08 +0900

    Mobile Menu Nav
    Added Hamburger and Mobile Menu Overlay.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
-
+const btn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu');
 // tabs menu event listener
 
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
+// Hamburger Button Event listener
+
+btn.addEventListener('click', navToggle);
 
 function onTabClick(e) {
   // Deactivate all tabs
@@ -26,4 +31,10 @@ function onTabClick(e) {
     .getElementById('panels')
     .getElementsByClassName(classString)[0]
     .classList.remove('hidden');
+}
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
 }
